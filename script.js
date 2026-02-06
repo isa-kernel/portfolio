33c4b84a4a2cff935101059f3f3f872c4b2fe3f0
isaack mburu <felixmburu03@gmail.com>
2026-02-07 02:38:57 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// DARK MODE TOGGLE
+const toggle = document.getElementById("darkToggle");
+toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    toggle.textContent =
+        document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
